b0734d251d559d89872e0678befa6c624a43f979
ilzat <ilzat.sadykov.2006@bk.ru>
2025-08-08 15:36:16 +0500

    finished

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins:{
+    Camera: {
+      permissions: ['camera', 'photos']
+    }
+  }
 };
 
 export default config;
